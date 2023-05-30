43f57c47af11f97617b61d9c44c7176159428773
ovaomnis <ovaomnis@gmail.com>
2023-05-30 13:43:10 +0600

    first commit

---
+++ git_commands.py
@@ -0,0 +1,15 @@
+
+
+git init -> .git/
+git remote add name url
+git pull
+git status
+git add <file_name> -> adds <file_name>
+	git add . -> adds everything in directory
+git commit -m 'message, comment' -> committing changes
+git branch -> listing branches
+	git branch <branch_name> -> creating new branch named as <branch_name>
+git checkout <branch_name> -> switching to <branch_name> if it branch exist, otherwise it creates and switches
+git push <branch_name> -> pushes new commits to remote repository
+git reset <file_name> -> removes file_name from index(staging area)
+
